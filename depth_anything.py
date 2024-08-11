b485ea50d9c8bd400c4b6d454b4f052134c9f971
nilor <lucas@nilor.cool>
2024-08-11 13:23:11 +0100

    import torch

--- depth_anything.py
+++ depth_anything.py
@@ -1,4 +1,16 @@
-
+import torch
+import torch
+import torch.nn.functional as F
+from torchvision import transforms
+import os
+from contextlib import nullcontext
+from contextlib import nullcontext
+try:
+    from accelerate import init_empty_weights
+    from accelerate.utils import set_module_tensor_to_device
+    is_accelerate_available = True
+except:
+    pass
 
 def process_depth():
     print("Processing depth")

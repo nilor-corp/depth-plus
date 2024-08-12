f3105b819648f86e971ee0381cf0802b031e7da5
Stephen <>
2024-08-12 15:13:43 +0800

    begin exr support for raft

--- optical_raft.py
+++ optical_raft.py
@@ -5,7 +5,7 @@ import argparse
 import torch
 import os
 import cv2
-from utils import get_bitsize_from_torch_type
+from utils import get_bitsize_from_torch_type, make_exr
 
 
 class DepthPlusOptical:
@@ -114,6 +114,10 @@ class DepthPlusOptical:
                         bitsize, nptype = get_bitsize_from_torch_type(torch.float32)
                         exr_frame = ((flo[:,:,[2,1,0]]/255.0) * bitsize).astype(nptype)
                         ##TODO Implement exr writing to exr_output_path
+                        exr_filename = os.path.join(exr_output_path, '{:04d}.exr'.format(frame_count))
+                        success = make_exr(exr_filename, exr_frame)
+                        if not success:
+                            raise ValueError("Error writing exr file")
                 frame_count += 1
                 prev_frame = curr_frame
             raw_video.release()

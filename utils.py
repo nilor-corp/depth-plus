adb12c9a2f3bf4af196af5c29ab4851c6a0b52d7
Stephen <>
2024-11-27 10:59:49 +0800

    formatting + logging

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ import Imath
 import os
 import cv2
 
+
 def load_torch_file(ckpt, safe_load=False, device=None):
     if device is None:
         device = torch.device("cpu")
@@ -13,8 +14,10 @@ def load_torch_file(ckpt, safe_load=False, device=None):
         sd = safetensors.torch.load_file(ckpt, device=device.type)
     else:
         if safe_load:
-            if not 'weights_only' in torch.load.__code__.co_varnames:
-                print("Warning torch.load doesn't support weights_only on this pytorch version, loading unsafely.")
+            if not "weights_only" in torch.load.__code__.co_varnames:
+                print(
+                    "Warning torch.load doesn't support weights_only on this pytorch version, loading unsafely."
+                )
                 safe_load = False
         if safe_load:
             pl_sd = torch.load(ckpt, map_location=device, weights_only=True)
@@ -28,19 +31,25 @@ def load_torch_file(ckpt, safe_load=False, device=None):
             sd = pl_sd
     return sd
 
+
 def get_bitsize_from_torch_type(torch_type):
     bit8 = 255
     bit16 = 65535
     bit32 = 4294967295
 
-    if(torch_type == torch.float8_e4m3fn or torch_type == torch.float8_e5m2 or torch_type == torch.float8_e4m3fnuz):
+    if (
+        torch_type == torch.float8_e4m3fn
+        or torch_type == torch.float8_e5m2
+        or torch_type == torch.float8_e4m3fnuz
+    ):
         return bit8, np.uint8
-    elif(torch_type == torch.float16):
+    elif torch_type == torch.float16:
         return bit16, np.uint16
-    elif(torch_type == torch.float32):
+    elif torch_type == torch.float32:
         return bit32, np.uint32
     else:
         raise ValueError("Invalid torch type get_bitsize_from_torch_type")
+
 
 def determine_image_type(tensor):
     if len(tensor.shape) == 2:
@@ -55,7 +64,15 @@ def determine_image_type(tensor):
         raise ValueError("Unsupported tensor shape")
     return num_channels, width, height
 
-def construct_output_paths(video_path, outdir, process_type, basename_string_concat="", is_png_8bit=True, is_exr_32bit=True):
+
+def construct_output_paths(
+    video_path,
+    outdir,
+    process_type,
+    basename_string_concat="",
+    is_png_8bit=True,
+    is_exr_32bit=True,
+):
     """
     Constructs output paths for video processing.
 
@@ -67,7 +84,7 @@ def construct_output_paths(video_path, outdir, process_type, basename_string_con
     :param is_exr_32bit: Whether the EXR output should be 32-bit.
     :return: A dictionary containing paths for mp4, png, and exr outputs.
     """
-    
+
     basename = os.path.splitext(os.path.basename(video_path))[0]
     if basename_string_concat:
         process_type = f"{process_type}_{basename_string_concat}"
@@ -77,21 +94,26 @@ def construct_output_paths(video_path, outdir, process_type, basename_string_con
     paths = {}
 
     # Construct paths for each output type
-    mp4_output_path = os.path.join(video_output_dir, 'mp4')
+    mp4_output_path = os.path.join(video_output_dir, "mp4")
     os.makedirs(mp4_output_path, exist_ok=True)
-    paths['mp4'] = os.path.join(mp4_output_path, f'{basename}_{process_type}.mp4')
-    
-    png_output_path = os.path.join(video_output_dir, 'png')
-    png_output_path = f"{png_output_path}_8bit" if is_png_8bit else f"{png_output_path}_16bit"
+    paths["mp4"] = os.path.join(mp4_output_path, f"{basename}_{process_type}.mp4")
+
+    png_output_path = os.path.join(video_output_dir, "png")
+    png_output_path = (
+        f"{png_output_path}_8bit" if is_png_8bit else f"{png_output_path}_16bit"
+    )
     os.makedirs(png_output_path, exist_ok=True)
-    paths['png'] = png_output_path
-    
-    exr_output_path = os.path.join(video_output_dir, 'exr')
-    exr_output_path = f"{exr_output_path}_32bit" if is_exr_32bit else f"{exr_output_path}_16bit"
+    paths["png"] = png_output_path
+
+    exr_output_path = os.path.join(video_output_dir, "exr")
+    exr_output_path = (
+        f"{exr_output_path}_32bit" if is_exr_32bit else f"{exr_output_path}_16bit"
+    )
     os.makedirs(exr_output_path, exist_ok=True)
-    paths['exr'] = exr_output_path
+    paths["exr"] = exr_output_path
 
     return paths
+
 
 def make_exr(filename_prefix, data=None):
     # File path handling
@@ -99,17 +121,13 @@ def make_exr(filename_prefix, data=None):
     if not useabs:
         current_folder = os.path.dirname(os.path.abspath(__file__))
         filename_prefix = os.path.join(current_folder, filename_prefix)
-
     print(f"len(data): {len(data.shape)}")
     print(f"data: {data}")
-
     # Determine if the data is single-channel or multi-channel
     num_channels, width, height = determine_image_type(data)
     print(f"width: {width}, height: {height}, num_channels: {num_channels}")
-
     # Prepare the data for writing
     exr_data = {}
-
     if num_channels == 1:
         # Single-channel image
         print(f"single-channel image")
@@ -117,78 +135,80 @@ def make_exr(filename_prefix, data=None):
     elif num_channels > 1:
         # Multi-channel image
         print(f"multi-channel image")
-        default_names = ["R", "G", "B", "A"] + [f"Channel{i}" for i in range(4, num_channels)]
+        default_names = ["R", "G", "B", "A"] + [
+            f"Channel{i}" for i in range(4, num_channels)
+        ]
         for k in range(num_channels):
             print(f"Channel {k}: {default_names[k]}")
-            # print(f"Data shape: {data.shape}")
-            # print(f"Data type: {data.dtype}")
-
             channel_data = data[:, :, k]
             exr_data[default_names[k]] = channel_data.astype(np.float32)
     else:
         raise ValueError("Unsupported tensor shape")
-
     print(f"EXR data: {exr_data}")
-
     # Write the EXR file
     return write_exr(filename_prefix, exr_data, width, height)
+
 
 def write_exr(writepath, exr_data, width, height):
     success = False
     try:
         # Create the EXR file header with dynamic channel names, using FLOAT for float32 data
         header = OpenEXR.Header(width, height)
-        header['channels'] = {name: Imath.Channel(Imath.PixelType(Imath.PixelType.FLOAT)) for name in exr_data.keys()}
+        header["channels"] = {
+            name: Imath.Channel(Imath.PixelType(Imath.PixelType.FLOAT))
+            for name in exr_data.keys()
+        }
 
         # Create the EXR file
-        print(f"create exr_file")
+        print(f"Creating EXR file at {writepath}")
         exr_file = OpenEXR.OutputFile(writepath, header)
 
         # Convert the channel data to bytes
-        print(f"channel_data")
+        print(f"Converting channel data to bytes")
         channel_data = {name: data.tobytes() for name, data in exr_data.items()}
 
         # Write the channel data to the EXR file
-        print(f"exr_file writePixels")
+        print(f"Writing pixels to EXR file")
         exr_file.writePixels(channel_data)
-        print(f"exr_file close")
+        print(f"Closing EXR file")
         exr_file.close()
 
         print(f"EXR file saved successfully to {writepath}")
 
         success = True
-        
+
     except Exception as e:
         print(f"Failed to write EXR file: {e}")
 
     return success
+
 
 def write_out_video_as_jpeg_sequence(video_path, filename, outdir=None):
     raw_video = cv2.VideoCapture(video_path)
     frame_rate = raw_video.get(cv2.CAP_PROP_FPS)
     frame_width = int(raw_video.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(raw_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    
 
-    if(outdir is None or outdir == ""):    
+    if outdir is None or outdir == "":
         outdir = r"temp-jpg-cache"
-        #strip extension from filename
+        # strip extension from filename
         filename_clean = os.path.splitext(filename)[0]
         outdir = os.path.join(outdir, filename_clean)
-    #make sure the directory exists
+    # make sure the directory exists
     if not os.path.exists(outdir):
         os.makedirs(outdir)
-    frame_count = 0 
+    frame_count = 0
     while raw_video.isOpened():
         ret, frame = raw_video.read()
         if not ret:
             break
-        jpeg_filename = os.path.join(outdir, '{:04d}.jpg'.format(frame_count))
+        jpeg_filename = os.path.join(outdir, "{:04d}.jpg".format(frame_count))
         success = cv2.imwrite(jpeg_filename, frame)
         if not success:
             print(f"Error writing {jpeg_filename}")
         frame_count += 1
     return outdir, frame_width, frame_height, frame_rate
+
 
 def delete_directory(directory):
     if os.path.exists(directory):
@@ -196,13 +216,18 @@ def delete_directory(directory):
             os.remove(os.path.join(directory, file))
         os.rmdir(directory)
 
+
 def get_int_sorted_dir_list(dir_path, extension=None):
     def extract_number(file_name):
-            return int(''.join(filter(str.isdigit, file_name)))
-    #interate through each image in jpg_dir
+        return int("".join(filter(str.isdigit, file_name)))
+
+    # interate through each image in jpg_dir
     file_list = sorted(
-        [f for f in os.listdir(dir_path) if (extension is None or f.lower().endswith(extension))],
-        key=extract_number)
+        [
+            f
+            for f in os.listdir(dir_path)
+            if (extension is None or f.lower().endswith(extension))
+        ],
+        key=extract_number,
+    )
     return file_list
-
-
